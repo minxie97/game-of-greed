102bba304cd5ddf0bd4d0e2d93a1ed9f983a0233
Ian Cargill <idcargill@gmail.com>
2021-12-29 19:47:29 -0800

    Tests passed lab_07

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,5 +1,6 @@
 from game_of_greed.game_logic import GameLogic
 from game_of_greed.banker import Banker
+from sys import exit
 
 class Game:
 
@@ -7,6 +8,7 @@ class Game:
         self.num_dice = 6
         self.rounds = 1
         self.bank = Banker()
+        self.playing = True
     
     def play(self, roller = GameLogic.roll_dice):
         self.roller = roller
@@ -15,8 +17,9 @@ class Game:
 
         yes_or_no = input('> ')
 
-        if yes_or_no == 'n':
+        if yes_or_no.lower() in ['n', 'no']:
             print('OK. Maybe another time')
+            exit()
         else:
             self.game_start()
 
@@ -35,26 +38,47 @@ class Game:
         score_tuple = tuple(map(int, list(response)))
         round_score = GameLogic.calculate_score(score_tuple)
         self.bank.shelf(round_score)
+
+    def print_score(self):
         print(f'You have {self.bank.shelved} unbanked points and {self.num_dice} dice remaining')
         print('(r)oll again, (b)ank your points or (q)uit:')
 
     def game_start(self):
-        print(f'Starting round {self.rounds}')
-        self.user_roll_dice()
-        print('Enter dice to keep, or (q)uit:')
 
-        keep_or_quit = input('> ')
+        while self.playing:
+            print(f'Starting round {self.rounds}')
+            self.user_roll_dice()
+            print('Enter dice to keep, or (q)uit:')
 
-        if keep_or_quit == 'q':
-            print('Thanks for playing. You earned 0 points')
+            keep_or_quit = input('> ')
 
-        else:
-            self.rounds += 1
-            self.num_dice -= len(keep_or_quit)
-            self.user_score(keep_or_quit)
-            response = input('> ')
-            if response == 'b':
-                self.bank.bank()
+            if keep_or_quit.lower() in ['q', 'quit']:
+                self.playing = False
+       
+            else: 
+                self.num_dice -= len(keep_or_quit)
+                self.user_score(keep_or_quit)
+                self.print_score()
+
+                response = input('> ')
+                if response.lower() in ['b', 'bank', 'give me money']:
+                    print(f'You banked {self.bank.shelved} points in round {self.rounds}')
+                    self.rounds += 1
+                    self.bank.bank()
+                    print(f'Total score is {self.bank.balance} points')
+                    self.num_dice = 6
+
+                elif response.lower() in ['q', 'quit']:
+                    self.playing = False
+
+                elif response.lower() == 'r':
+                    print('Doing R stuff')
+                    
+        print(f'Thanks for playing. You earned {self.bank.balance} points')
+        exit()
 
 
-
+if __name__ == '__main__':
+    game = Game()
+    game.play()
+    
