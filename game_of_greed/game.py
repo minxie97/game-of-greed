ed828a48f9d62bbe492a552378c49dd2647455dd
Ian Cargill <idcargill@gmail.com>
2021-12-29 18:30:54 -0800

    init setup & test passed: Quitter

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,2 +1,35 @@
+from sys import exit
+from game_of_greed.game_logic import GameLogic
+
+roller = GameLogic.roll_dice
+
+welcome_intro = '''Welcome to Game of Greed
+(y)es to play or (n)o to decline
+> '''
+
+
 class Game:
-    pass
+    def __init__(self):
+        pass
+
+    def play(self, roller):
+        choice = input(welcome_intro)
+
+        if choice.lower() in ['n', 'no']:
+            print('OK. Maybe another time')
+            exit()
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.play(roller)
