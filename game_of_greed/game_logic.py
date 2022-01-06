6bd04cf23ed4b78fc20342e10b0fce05fe74eba7
Ian Cargill <idcargill@gmail.com>
2022-01-05 18:59:08 -0800

    GameLogic.validate_keepers

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -22,3 +22,40 @@ class GameLogic:
                     if side in [1, 5]:
                         score += 100 * count if side == 1 else 50 * count
         return score
+    
+    @staticmethod
+    def dice_tuple_to_dict(tup):
+        result_dict = {}
+        for i in list(tup):
+            if  i in result_dict: 
+                result_dict[i] += 1
+            else:
+                result_dict[i] = 1
+        return result_dict
+
+    @staticmethod
+    def validate_keepers(roll, keepers):
+        rolled = list(roll)
+        roll_count = GameLogic.dice_tuple_to_dict(roll)
+
+        try:
+            numbers = [int(i) for i in list(keepers)]
+
+            if len(numbers) > len(rolled):
+                return False
+
+            for num in numbers:
+                num_count = numbers.count(num)
+                print(num_count)
+                if num_count > roll_count[num]:
+                    return False
+            return True
+
+        except Exception as e:
+            return False
+
+if __name__ == '__main__':
+    roll = (1,2, 5, 5,)
+    keepers = (1,)
+    x= GameLogic.validate_keepers(roll, keepers)
+    print(x)
